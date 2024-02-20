f0429594254cb1fc114905e66d20ffd09c70eb9d
Christy <Edgardchristianne@gmail.com>
2024-02-20 13:25:54 -0500

    First commit restart passion project in spring

---
+++ CluelessCloset-app/src/main/java/com/javaguides/CluelessClosetapp/CluelessClosetAppApplication.java
@@ -0,0 +1,13 @@
+package com.javaguides.CluelessClosetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluelessClosetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluelessClosetAppApplication.class, args);
+	}
+
+}
---
+++ CluelessCloset-app/src/test/java/com/javaguides/CluelessClosetapp/CluelessClosetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.CluelessClosetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluelessClosetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
