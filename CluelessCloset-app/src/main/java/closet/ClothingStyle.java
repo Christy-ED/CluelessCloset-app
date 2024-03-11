030b68c8c8d1be5ed97f67fc6f83175f4328179e
Christy <Edgardchristianne@gmail.com>
2024-03-11 14:15:45 -0400

    Working in the colthingStyle entity

--- CluelessCloset-app/src/main/java/closet/ClothingStyle.java
+++ CluelessCloset-app/src/main/java/closet/ClothingStyle.java
@@ -2,6 +2,15 @@ package closet;
 
 public class ClothingStyle {
     int clothingStyle;
+
+    public String getCasual() {
+        return casual;
+    }
+
+    public void setCasual(String casual) {
+        this.casual = casual;
+    }
+
     String casual;
 
     String formal;
